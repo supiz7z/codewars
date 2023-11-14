c22fe5fb6d37488516ae769d180237b674d31d8a
supiz7z <burubulka12@gmail.com>
2023-11-14 18:12:11 +0300

    [edit] some katas

--- kata_6/dominant_primes/solve.go
+++ kata_6/dominant_primes/solve.go
@@ -1,25 +1,14 @@
 package dominant_primes
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func IsPrime(n int) bool {
-	//if n < 2 {
-	//	return false
-	//}
-	//for i := 2; i <= int(math.Sqrt(float64(n))); i++ {
-	//	if n%i == 0 {
-	//		return false
-	//	}
-	//}
-	//return true
-	for i := 2; i*i < n+i; i++ { // этот алгоритм взят из ответов, мой выше закомментирован. Задача была сдана с моим)
+	for i := 2; i*(i-1) < n; i++ {
 		if n%i == 0 {
 			return false
 		}
 	}
-	return true
+	return n > 1
 }
 
 func Solve(a, b int) int {
