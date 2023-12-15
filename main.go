8f3ebee9aa9fe702a3a21e553bac6153acb6d1ce
supiz7z <burubulka12@gmail.com>
2023-12-15 21:42:57 +0300

    [edit] some katas

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-import "codewars/kata_5/numbers_of_measurements_to_spot_the_counterfeit_coin"
+import (
+	"codewars/kata_6/numbers_of_measurements_to_spot_the_counterfeit_coin"
+)
 
 func main() {
 	numbers_of_measurements_to_spot_the_counterfeit_coin.Start()
